635af3370aa3f039711e55980178f6098c04cba0
Nikolay Parapanov <n.parapanoff@gmail.com>
2022-12-12 23:04:18 +0200

    upload on 22-12-12 ver. 2

--- tri/tri/marketplace/urls.py
+++ tri/tri/marketplace/urls.py
@@ -3,7 +3,7 @@ from tri.marketplace.views import MarketplaceListView, MarketplaceDetailsView, M
 
 urlpatterns = (
     path('', MarketplaceListView.as_view(), name='marketplace list'),
-    path('new-item/', MarketplaceItemCreateView.as_view(), name='marketplace new item'),
-    path('details/<int:pk>/', MarketplaceDetailsView.as_view(), name='marketplace details item'),
-        # path('/', MarketDisplayListView.as_view(), name='market display list'),
+    path('add/', MarketplaceItemCreateView.as_view(), name='marketplace item add'),
+    path('details/<int:pk>/', MarketplaceDetailsView.as_view(), name='marketplace item details'),
+
     )
--- tri/tri/marketplace/views.py
+++ tri/tri/marketplace/views.py
@@ -1,6 +1,7 @@
-from django.urls import reverse_lazy
+from django import forms
 from django.views import generic as views
 from tri.marketplace import models as models
+from tri.marketplace.models import MarketItems
 
 
 class MarketplaceListView(views.ListView):
@@ -17,7 +18,6 @@ class MarketplaceListView(views.ListView):
 
         pattern = self.__get_pattern()
         if pattern:
-
             queryset = queryset.filter(name__icontains=pattern.lower())
         return queryset
 
@@ -27,15 +27,50 @@ class MarketplaceListView(views.ListView):
 
 
 class MarketplaceDetailsView(views.DetailView):
-    template_name = 'marketplace/marketplace-detail-pk.html'
+    template_name = 'marketplace/marketplace-detail.html'
     model = models.MarketItems
 
+class ItemCreateForm(forms.ModelForm):
+    class Meta:
+        model = models.MarketItems
+        exclude = ['user_id']
+        labels = {
+            'name': 'name',
+            'description': 'description',
+            'price': 'price:',
+            'type': 'Date of Birth',
+        }
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Item name'
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'placeholder': 'Short description of the item'
+                }
+            ),
+            'price': forms.NumberInput(
+                attrs={
+                    'placeholder': 'Price (in EUR)',
+                }
+            ),
+            'type': forms.Select(
+                attrs={
+                    'placeholder': 'Select from the dropdown options',
+                }
+            )
+        }
 
-class MarketplaceItemCreateView(views.CreateView):
-    template_name = 'marketplace/marketplace-item-create.html'
+class MarketplaceItemCreateView(views.CreateView, ItemCreateForm):
+    template_name = 'marketplace/marketplace-item-add.html'
     model = models.MarketItems
-    fields = '__all__'
-    success_url = 'marketplace/marketplace-list/'
+    # fields = ['name', 'description', 'price', 'type']
+
+    form_class = ItemCreateForm
+
+    # success_url = reverse_lazy('marketplace item details')
 
     # def get_success_url(self):
     #     created_object = self.object
