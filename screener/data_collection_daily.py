6c144e82b6b5b726b0e775d8ef58da407b239bed
nikolay <n.parapanoff@gmail.com>
2023-12-14 23:16:46 +0200

    stocks screener project with flask

---
+++ screener/data_collection_daily.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import yfinance as yf
+import datetime as dt
+
+
+def data_collection_daily():
+    today = str(dt.date.today())
+    with open('database/tickers_list/tickers_list.csv') as f:
+        companies = f.read().splitlines()
+        all_symbols_df = pd.DataFrame()
+        for company in companies:
+            symbol = company.split(',')[0]
+            current_symbol_df = yf.download(symbol, start='2023-01-01', end=today)
+            current_symbol_df.insert(0, 'Ticker' , str(symbol))
+            all_symbols_df = pd.concat([all_symbols_df, current_symbol_df])
+
+        print(all_symbols_df)
+        # all_symbols_df.to_csv('database/daily/all_symbols_daily.csv')
