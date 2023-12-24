fb91034969532ef28e3f8f90395cc07ba22f626a
nikolay <n.parapanoff@gmail.com>
2023-12-24 23:27:59 +0200

    refactoring the set up for better visual alignment

--- stocks_screener/step_0_app.py
+++ stocks_screener/step_0_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
-import step_2_adding_preselected_patterns_from_talib, check_for_setups, step_1_data_collection, step_3_last_data, patterns, test, \
-    filter_setups, visualization
+import step_1_data_collection , step_2_adding_preselected_patterns_from_talib, step_4_check_for_setups, step_1_data_collection, step_3_last_data, patterns, test, \
+    step_5_filter_setups, step_6_visualization
 
 app = Flask(__name__)
 
@@ -12,62 +12,58 @@ def index():
 
     return render_template('index.html')
 
-
 @app.route('/general_daily_scanner')
 def general_daily_scanner_route():
 
     # This procedure is to perform all of the daily scanning activities at one page (incl. visualization at the end):
 
-    data_collection.data_collection_daily()
-    adding_preselected_patterns_from_talib.adding_preselected_patterns_to_df_from_ta_lib()
-    last_data.last_day_data()
-    check_for_setups.check_for_setups_in_last_days()
-    filter_setups.filter_setups_for_summary()
-    details = visualization.visualize_predefined_dataframe()
+    step_1_data_collection.data_collection_daily()
+    step_2_adding_preselected_patterns_from_talib.adding_preselected_patterns_to_df_from_ta_lib()
+    step_3_last_data.last_day_data()
+    step_4_check_for_setups.check_for_setups_in_last_days()
+    step_5_filter_setups.filter_setups_for_summary()
+    details = step_6_visualization.visualize_predefined_dataframe()
 
     return render_template('general_daily_scanner.html', details=details)
 
 
+@app.route('/symbol/')
+def scanner_for_specific_symbol():
 
+
+    return
 
 @app.route('/collect_data')
 def snapshot():
-    collected = data_collection_daily.data_collection_daily()
+    collected = step_1_data_collection.data_collection_daily()
     print('data collected')
     return collected
 
 
 @app.route('/enrich_data')
 def enrich_data():
-    adding_preselected_patterns_from_talib.adding_preselected_patterns_to_df_from_ta_lib()
+    step_2_adding_preselected_patterns_from_talib.adding_preselected_patterns_to_df_from_ta_lib()
 
     return 'data enriched'
 
 
 @app.route('/last_day_data')
 def last_day():
-    last_day_data.last_day_data()
+    step_3_last_data.last_day_data()
 
     return 'last day data filtered'
 
 
 @app.route('/check_for_setups')
 def check():
-    check_for_setups.check_for_setups_in_last_days()
+    step_4_check_for_setups.check_for_setups_in_last_days()
 
     return 'check for setups has been done'
 
 
-@app.route('/test')
-def test1():
-    test.test_code()
-
-    return 'test code'
-
-
 @app.route('/filter')
 def filter_setups1():
-    filter_setups.filter_setups_for_summary()
+    step_5_filter_setups.filter_setups_for_summary()
 
     return 'filtered setups'
 
@@ -75,7 +71,16 @@ def filter_setups1():
 @app.route('/visualize')
 def visualize1():
 
-    details = visualization.visualize_predefined_dataframe()
+    details = step_6_visualization.visualize_predefined_dataframe()
 
     # return 'viz'
     return render_template('general_daily_scanner.html', details=details)
+
+
+
+
+@app.route('/test')
+def test1():
+    test.test_code()
+
+    return 'test code'
