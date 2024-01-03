250ebe03854e1ccc84d45f83be98b0afe4a2fe13
nikolay <n.parapanoff@gmail.com>
2024-01-03 11:52:52 +0200

    adding Bollinger bands and atr indicators for the individual in depth analysis function + link to the general scanner

--- stocks_screener/symbol_analysis.py
+++ stocks_screener/symbol_analysis.py
@@ -16,8 +16,14 @@ def individual_symbol_analysis(symbol):
     current_symbol_hourly_df = yf.download(symbol, interval="1h", start=twenty_months_ago, end=today)
     current_symbol_hourly_df.insert(0, 'Ticker', str(symbol))
 
-    print(current_symbol_hourly_df)
+    bb_indicator = BollingerBands(current_symbol_hourly_df['Adj Close'])
+    current_symbol_hourly_df['upper_band'] = bb_indicator.bollinger_hband()
+    current_symbol_hourly_df['lower_band'] = bb_indicator.bollinger_lband()
+    current_symbol_hourly_df['ma_band'] = bb_indicator.bollinger_mavg()
 
-    bb_indicator = BollingerBands()
+    atr_indicator = AverageTrueRange(current_symbol_hourly_df['High'], current_symbol_hourly_df['Low'],current_symbol_hourly_df['Adj Close'])
+    current_symbol_hourly_df['atr'] = atr_indicator.average_true_range()
+
+    print(current_symbol_hourly_df)
 
     return
